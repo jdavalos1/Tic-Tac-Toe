d51bddcf76e9c2ca178ab1b5bfd8478026e95db5
Juan Davalos <jcharlied13@gmail.com>
2021-05-25 20:36:10 -0700

    properly added the material for draw

--- Tic Tac Toe/Assets/Scripts/In-Game Ui/InGameUI.cs
+++ Tic Tac Toe/Assets/Scripts/In-Game Ui/InGameUI.cs
@@ -1,30 +1,59 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class InGameUI : MonoBehaviour
 {
     public Text turnText;
-    public GameObject drawPanel;
+    public GameObject endGamePanel;
+    public Material[] panelMats;
 
+    /// <summary>
+    /// Swap text on the player's turn
+    /// </summary>
+    /// <param name="oTurn">Who plays next?</param>
     public void SwapTurn(bool oTurn)
     {
         turnText.text = oTurn ? "O" : "X";
         turnText.text += "\'s Turn";
     }
 
+    public void ShowWin(bool oWin)
+    {
+        var t = endGamePanel.transform.Find("Title");
+        t.GetComponent<Text>().text = "X Wins!!";
+        endGamePanel.GetComponent<Image>().material = panelMats[1];
+
+        if (oWin)
+        {
+            t.GetComponent<Text>().text = "O Wins!!";
+            endGamePanel.GetComponent<Image>().material = panelMats[2];
+        }
+    }
+    /// <summary>
+    /// Show the panel to signify a draw
+    /// </summary>
     public void ShowDraw()
     {
-        drawPanel.SetActive(true);
+        var t = endGamePanel.transform.Find("Title");
+        t.GetComponent<Text>().text = "Draw: No One Wins";
+        endGamePanel.GetComponent<Image>().material = panelMats[0];
+        endGamePanel.SetActive(true);
     }
 
-    public void ReturnMainMenu()
-    {
-        SceneManager.LoadScene("Intro");
-    }
-
+    /// <summary>
+    /// Load new game after an event
+    /// </summary>
     public void NewGame()
     {
         SceneManager.LoadScene("TicTacToe");
+    }
+    /// <summary>
+    /// Return to the main menu
+    /// </summary>
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene("Intro");
     }
 }
--- Tic Tac Toe/Assets/Scripts/Tic-Tac-Toe/Board/TilePlace.cs
+++ Tic Tac Toe/Assets/Scripts/Tic-Tac-Toe/Board/TilePlace.cs
@@ -21,7 +21,7 @@ public class TilePlace : MonoBehaviour
         CurrentPiece = control.oTurn ? 'o' : 'x';
 
         // Prepare a new piece and set it's parent as the current tile
-        GameObject playerPiece = Instantiate(Resources.Load<GameObject>($"Letters/{CurrentPiece}"));
+        GameObject playerPiece = Instantiate(Resources.Load<GameObject>($"Prefabs/Letters/{CurrentPiece}"));
         playerPiece.transform.parent = transform;
         playerPiece.transform.localPosition= new Vector3(control.oTurn ? 0.4f : 0.35f, 0, -0.3f);
         control.CheckWin();
